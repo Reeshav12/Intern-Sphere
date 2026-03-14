de3d6b26c79f565f0efda0ddf9dd48436f2346f6
Reeshav12 <rishab00raj01@gmail.com>
2026-03-14 17:19:42 +0530

    Defer app initialization in Vercel entrypoint

--- api/index.py
+++ api/index.py
@@ -6,7 +6,4 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
-from app import app, initialize_application
-
-
-initialize_application()
+from app import app
